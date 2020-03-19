743b188efb5b0b82063455bcd9a1c0e7ab0002f4
kimcodingvv <junghun1997@naver.com>
2020-03-20 04:20:46 +0900

    ps 20.03.19

--- BOJ/16986_인싸들의 가위바위보.cpp
+++ BOJ/16986_인싸들의 가위바위보.cpp
@@ -1,44 +1,46 @@
 #include <bits/stdc++.h>
 #define FIO ios::sync_with_stdio(0), cin.tie(0),cout.tie(0)
-#define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define FOR(i, a, b) for(int i=a;i<=b;i++)
 using namespace std;
 
 int main() {
 	FIO;
-	int n, k; cin >> n >> k;
-	int t[10][10] = {};
+	int n, k;
+	cin >> n >> k;
+	int t[10][10];
 	FOR(i, 1, n) FOR(j, 1, n) cin >> t[i][j];
-	int bc[4][21];
-	FOR(i, 1, 20) cin >> bc[2][i];
-	FOR(i, 1, 20) cin >> bc[3][i];
-	bool v[10] = {};
-	int w[4] = {}, c[4] = {};
-    function <bool(int,int,int)> solve = [&](int i, int f, int s){
-		++c[f], ++c[s];
-        if(w[1] == k) return 1;
-        if(w[2] == k || w[3] == k) return --c[f], --c[s], 0;
-        if(f != 1 && s != 1){
-            if(t[bc[f][c[f]]][bc[s][c[s]]] == 2 || (t[bc[f][c[f]]][bc[s][c[s]]] == 1 && f > s)){
-                if(++w[f] && solve(i + 1, f, 1)) return 1;
-                else return --w[f], --c[f], --c[s], 0;
-            }
-            if(++w[s] && solve(i + 1, s, 1)) return 1;
-            return --w[s], --c[f], --c[s], 0;
-        }
-        FOR(j,1,n){
-            if(v[j]) continue;
-			v[j] = 1;
-			if (t[j][bc[(f & s) + 2][c[(f & s) + 2]]] == 2) {
-				if (++w[1] && solve(i + 1, 1, 6 - f - s)) return 1;
-				--w[1];
+	int rps[3][21], cnt[3] = {}, p[3] = {};
+	bool v[10];
+	FOR(i, 1, 20) cin >> rps[1][i];
+	FOR(i, 1, 20) cin >> rps[2][i];
+
+	function<int(int, int, int)> ans = [&](int i, int f, int s) {
+		if (p[0] == k) return 1;
+		if (p[1] == k || p[2] == k) return 0;
+		cnt[f]++, cnt[s]++;
+		int ff = rps[f][cnt[f]], ss = rps[s][cnt[s]];
+		if (f && s) {
+			int win = (t[ff][ss] == 2 || (t[ff][ss] == 1 && f > s)) ? f : s;
+			p[win]++;
+			if (ans(i + 1, win, 0)) return 1;
+			p[win]--;
+		}
+		else {
+			FOR(j, 1, n) {
+				if (v[j]) continue;
+				v[j] = 1;
+				int x = rps[f | s][cnt[f | s]];
+				int win = (t[j][x] == 2) ? 0 : f | s;
+				p[win]++;
+				if (ans(i + 1, win, 3 - f - s)) return 1;
+				p[win]--;
+				v[j] = 0;
 			}
-			else if (++w[(f & s) + 2] && solve(i + 1, (f & s) + 2, 6 - f - s)) return 1;
-			else --w[(f & s) + 2];
-			v[j] = 0;
-        }
-		--c[f], --c[s];
-        return 0;
-    };
-    cout << solve(1,1,2) << '\n';
-    return 0;
+		}
+		cnt[f]--, cnt[s]--;
+		return 0;
+	};
+
+	cout << ans(1, 0, 1) << '\n';
+	return 0;
 }
--- BOJ/3954_Brainfxxk 인터프리터.cpp
+++ BOJ/3954_Brainfxxk 인터프리터.cpp
@@ -1,6 +1,7 @@
 #include <bits/stdc++.h>
 #define FIO ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
 #define FOR(i,a,b) for(int i=a;i<=b;i++)
+#define MAX 50000000
 using namespace std;
 
 int main() {
@@ -20,21 +21,27 @@ int main() {
 				st.pop();
 			}
 		}
-		int p = 0, c = 0, cnt = 0, chk = -1;
+		int p = 0, c = 0, cnt = 0, chk = 1e9, flag = 0;
 		FOR(i, 0, m - 1) {
 			if (a[i] == '-') arr[p] = (arr[p] + 255) % 256;
-			if (a[i] == '+') arr[p] = (arr[p] + 1) % 256;
-			if (a[i] == '<') p = (p - 1 + n) % n;
-			if (a[i] == '>') p = (p + 1) % n;
-			if (a[i] == '[' && !arr[p]) i = idx[i];
-			if (a[i] == ']' && arr[p]) chk = max(chk, i), i = idx[i];
-			if (a[i] == ',') arr[p] = (c == k ? 255 : (int)b[c++]);
-			if (++cnt == 50000000) {
-				cout << "Loops " << idx[chk] << ' ' << chk << '\n';
+			else if (a[i] == '+') arr[p] = (arr[p] + 1) % 256;
+			else if (a[i] == '<') p = (p - 1 + n) % n;
+			else if (a[i] == '>') p = (p + 1) % n;
+			else if (a[i] == '[') {
+				if (MAX <= cnt)
+					chk = min(chk, i);
+				if (MAX * 2 <= cnt)
+					flag = 1;
+				if (!arr[p]) i = idx[i] - 1;
+			}
+			else if (a[i] == ']' && arr[p]) i = idx[i] - 1;
+			else if (a[i] == ',') arr[p] = (c == k ? 255 : (int)b[c++]);
+			if (++cnt && flag) {
+				cout << "Loops " << chk << ' ' << idx[chk] << '\n';
 				break;
 			}
 		}
-		if(cnt < 50000000) cout << "Terminates\n";
+		if (!flag) cout << "Terminates\n";
 	}
 	return 0;
 }
